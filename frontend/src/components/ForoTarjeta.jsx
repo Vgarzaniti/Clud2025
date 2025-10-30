c6a06ae8b2cb45c8c432c5fb6099b71e64fd2d95
Sofia Raggi <sraggi@alu.frlp.utn.edu.ar>
2025-10-30 13:50:46 -0300

    Desarrollo de buscador de foros en Home

--- frontend/src/components/ForoTarjeta.jsx
+++ frontend/src/components/ForoTarjeta.jsx
@@ -9,10 +9,10 @@ export default function ForoTarjeta({ foro }) {
   return (
     <div
       onClick={() => navigate(`/foro/${foro.id}`)}
-      className="bg-panel p-4 rounded-2xl border border-gray-700 shadow-md cursor-pointer hover:bg-gray-800 transition"
+      className="bg-panel p-5 rounded-2xl border border-gray-700 shadow-md cursor-pointer hover:bg-gray-800 transition"
     >
       <h2 className="text-lg font-semibold text-white">{foro.titulo}</h2>
-      <p className="text-gray-300 text-sm mb-2">{foro.descripcion}</p>
+      <p className="text-gray-400 text-sm mb-2 mt-2">Materia: {foro.materia}</p>
       <div className="flex justify-between text-gray-400 text-sm">
         <span>Autor: {foro.autor}</span>
         <span>Respuestas: {foro.respuestas}</span>
