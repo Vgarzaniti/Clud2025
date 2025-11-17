ac0f887af15bb861d3aa7ec9b10695217a79915d
Valentin Garzaniti <valenxity@gmail.com>
2025-11-16 23:57:38 -0300

    endpoint de delete y edit

---
+++ backend/app/views/delete.py
@@ -0,0 +1,13 @@
+from django.db.models.signals import post_delete
+from django.dispatch import receiver
+from ..models import RespuestaArchivo
+from cloudinary.uploader import destroy
+
+@receiver(post_delete, sender=RespuestaArchivo)
+def eliminar_archivo_cloudinary(sender, instance, **kwargs):
+    try:
+        # Extraer public_id del archivo
+        public_id = instance.archivo.split('/upload/')[-1].rsplit('.', 1)[0]
+        destroy(public_id)
+    except Exception:
+        pass  # ignora si falla Cloudinary
