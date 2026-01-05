1c4ae536bb41d3b8c2a4a2ea537063628386c689
Sofia Raggi <sraggi@alu.frlp.utn.edu.ar>
2026-01-04 21:22:23 -0300

    Fijo de informacion del usuario en perfil

--- frontend/src/pages/Perfil.jsx
+++ frontend/src/pages/Perfil.jsx
@@ -167,7 +167,7 @@ export default function Perfil() {
   return (
     <div className="max-w-7xl mx-auto mt-10 px-6 text-texto grid grid-cols-1 lg:grid-cols-3 gap-6">
       {/* Panel lateral usuario */}
-      <aside className="bg-perfilPanel p-8 pt-20 mt-10 rounded-2xl border border-gray-700 relative w-72 mx-auto">
+      <aside className="bg-perfilPanel p-8 pt-20 mt-10 rounded-2xl border border-gray-700 relative w-72 mx-auto self-start">
         <div className="shadow-gray-900 shadow-lg w-24 h-24 bg-green-500 rounded-full flex items-center justify-center text-2xl font-bold text-fondo absolute -top-12 left-1/2 transform -translate-x-1/2">
           {usuario.nombre
             ? usuario.nombre
