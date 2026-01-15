8e1acd1e0e86656bd33d3afb3a43ecc033c48a83
Sofia Raggi <sraggi@alu.frlp.utn.edu.ar>
2026-01-15 11:40:50 -0300

    Implementacion de service para usuario

---
+++ frontend/src/services/userService.js
@@ -0,0 +1,28 @@
+import api from './api';
+
+export const userService = {
+    
+    login: async (email, password) => {
+        const res = await api.post("/login/", {
+        login: true,
+        email,
+        password,
+        });
+        return res.data;
+    },
+
+    register: async (data) => {
+        const res = await api.post("/login/", data);
+        return res.data;
+    },
+
+    me: async () => {
+        const res = await api.get("/usuario/me/");
+        return res.data;
+    },
+
+    logout: async () => {
+        await api.post("/logout/");
+    },
+}
+
