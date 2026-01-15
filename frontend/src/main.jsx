000ad14c4ccfba101b37d2e208fc2018fdd98d1d
Sofia Raggi <sraggi@alu.frlp.utn.edu.ar>
2026-01-15 12:11:50 -0300

    Desarrollo de rutas privadas

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { AuthContext } from './context/AuthContext.jsx'
 import App from './App.jsx'
 import './input.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <AuthContext>
+      <App />
+    </AuthContext>
   </StrictMode>,
 )
