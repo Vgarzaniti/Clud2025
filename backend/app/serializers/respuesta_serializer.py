2602a20215a93d9fe8913c3768b8c117fd8c7991
Valentin Garzaniti <valenxity@gmail.com>
2026-02-01 20:24:55 -0300

    fix puntaje

--- backend/app/serializers/respuesta_serializer.py
+++ backend/app/serializers/respuesta_serializer.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from ..models import Respuesta, RespuestaArchivo, Puntaje
+from ..models import Respuesta, RespuestaArchivo, Puntaje, Usuario
 
 
 class PuntajeRespuestaSerializer(serializers.ModelSerializer):
